04573cd12bd20ae2084b47a5c5f4d47114f5c091
Alexandrov Vladimir Morozov <81023608+YamiyoshinLevi@users.noreply.github.com>
2024-09-15 02:00:28 +0800

    build!!!

--- scenes/core.gd
+++ scenes/core.gd
@@ -3,6 +3,8 @@ extends Node2D
 # Initialize
 var x = 0
 var y = 0
+var moveX = "+"
+var moveY = "+"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,5 +12,22 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += 50 * delta
+	if position.x >= 1280:
+		moveX = "-"
+	if position.y >= 720:
+		moveY = "-"
+	if position.x <= 0:
+		moveX = "+"
+	if position.y <= 0:
+		moveY = "+"
+	if moveX == "+":
+		position.x += 144 * delta
+	if moveX == "-":
+		position.x -= 144 * delta
+	if moveY == "+":
+		position.y += 144 * delta
+	if moveY == "-":
+		position.y -= 144 * delta
 	print(position)
+	print(moveX)
+	print(moveY)
